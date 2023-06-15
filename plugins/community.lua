76ba6d8a358c2f57d7c06693bcca83f2006ffd4a
Dominic Lacaille <dlacaille@solution-tek.com>
2023-06-15 12:23:39 -0400

    Add overseer

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,20 +6,24 @@ return {
     { import = "astrocommunity.motion.mini-move" },
     { import = "astrocommunity.motion.mini-surround" },
     { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+    { import = "astrocommunity.code-runner.overseer-nvim" },
     -- Customize plugins further
     {
         "catppuccin",
         opts = {
-            transparent_background = true,
-            integrations = {
-                native_lsp = {
-                    enabled = true,
-                    underlines = {
-                        errors = { "undercurl" },
-                        hints = { "undercurl" },
-                        warnings = { "undercurl" },
-                        information = { "undercurl" },
-                    },
+            native_lsp = {
+                enabled = true,
+                virtual_text = {
+                    errors = { "italic" },
+                    hints = { "italic" },
+                    warnings = { "italic" },
+                    information = { "italic" },
+                },
+                underlines = {
+                    errors = { "undercurl" },
+                    hints = { "undercurl" },
+                    warnings = { "undercurl" },
+                    information = { "undercurl" },
                 },
             },
         },
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
-        auto_install = true, -- Requires tree-sitter-cli to be installed. `npm i -g tree-sitter-cli`
-        ensure_installed = { "lua" },
+        ensure_installed = { "lua", "typescript" },
     },
 }
