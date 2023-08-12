952372c087650b155d6656a8a16328c8954f5629
Dominic Lacaille <dominic@lacaille.app>
2023-08-11 22:15:24 -0400

    Remove deprecated catppuccin config

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,25 +4,5 @@ return {
     { import = "astrocommunity.motion.mini-move" },
     { import = "astrocommunity.motion.mini-surround" },
     { import = "astrocommunity.code-runner.overseer-nvim" },
-    -- Customize plugins further
-    {
-        "catppuccin",
-        opts = {
-            native_lsp = {
-                enabled = true,
-                virtual_text = {
-                    errors = { "italic" },
-                    hints = { "italic" },
-                    warnings = { "italic" },
-                    information = { "italic" },
-                },
-                underlines = {
-                    errors = { "undercurl" },
-                    hints = { "undercurl" },
-                    warnings = { "undercurl" },
-                    information = { "undercurl" },
-                },
-            },
-        },
-    },
+    { import = "astrocommunity.utility.noice-nvim" },
 }
