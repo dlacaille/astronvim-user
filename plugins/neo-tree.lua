7debf14a72337808f8b15bb01fc57e1100916403
Dominic Lacaille <dominic@lacaille.app>
2023-08-11 22:14:30 -0400

    Make neo-tree a floating window instead of auto-closing

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,22 +1,28 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+        popup_border_style = "single", -- Shows a border around the window
         filesystem = {
             filtered_items = {
-                visible = true,
+                visible = true, -- Shows hidden files by default
             },
         },
         source_selector = {
-            default_source = "filesystem"
+            winbar = false, -- Hide the tab bar as there is only 1 tab
+            sources = {
+                -- Remove all sources but filesystem
+                { source = "filesystem", display_name = "File" },
+            }
         },
-        event_handlers = {
-            {
-                event = "file_opened",
-                handler = function()
-                    --auto close
-                    require("neo-tree").close_all()
-                end,
-            },
-        },
+        window = {
+            position = 'float', -- Make the window floating by default
+            popup = {
+                size = { height = "95%", width = 60 }, -- Takes the whole height but 60 columns width
+                position = {
+                    row = "50%", -- Center vertically
+                    col = 2
+                },
+            }
+        }
     },
 }
