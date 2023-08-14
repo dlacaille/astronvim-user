4780337fbe779f2f4601f222f1b83a2089e58b5b
Dominic Lacaille <dlacaille@solution-tek.com>
2023-08-14 15:15:29 -0400

    Add mini-basics

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,7 @@
 return {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.catppuccin" }, -- Awesome color scheme
+    { import = "astrocommunity.motion.mini-basics" }, -- Basics for window nav and move in insert with M-hkjl
     { import = "astrocommunity.motion.mini-move" }, -- Move text selections
     { import = "astrocommunity.motion.mini-surround" }, -- Surround text
     { import = "astrocommunity.code-runner.overseer-nvim" }, -- Manage and run tasks
