ce872dbd5a76f3135b5e1ba0b2e2b26b68be043b
Dominic Lacaille <dlacaille@solution-tek.com>
2023-08-14 15:34:19 -0400

    Formatting

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,10 +1,10 @@
 return {
     "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.catppuccin" }, -- Awesome color scheme
-    { import = "astrocommunity.motion.mini-basics" }, -- Basics for window nav and move in insert with M-hkjl
-    { import = "astrocommunity.motion.mini-move" }, -- Move text selections
-    { import = "astrocommunity.motion.mini-surround" }, -- Surround text
+    { import = "astrocommunity.colorscheme.catppuccin" },    -- Awesome color scheme
+    { import = "astrocommunity.motion.mini-basics" },        -- Basics for window nav and move in insert with M-hkjl
+    { import = "astrocommunity.motion.mini-move" },          -- Move text selections
+    { import = "astrocommunity.motion.mini-surround" },      -- Surround text
     { import = "astrocommunity.code-runner.overseer-nvim" }, -- Manage and run tasks
-    { import = "astrocommunity.utility.noice-nvim" }, -- Rice cmdline / errors / notifications
-    { import = "astrocommunity.startup.fsplash-nvim" }, -- Floating window splash screen
+    { import = "astrocommunity.utility.noice-nvim" },        -- Rice cmdline / errors / notifications
+    { import = "astrocommunity.startup.fsplash-nvim" },      -- Floating window splash screen
 }
